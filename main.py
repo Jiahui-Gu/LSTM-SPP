ac2d6355d26577cf5eb2faee579a958d81890873
Jayden <scalarequilibrium@gmail.com>
2020-07-07 23:57:28 +0800

    Added Project Folder

--- main.py
+++ main.py
@@ -3,18 +3,14 @@ import pandas as pd
 from datetime import datetime as dt
 from math import sqrt
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-import pickle
 
 
-# Date Parser
 def parser(date):
     return dt.strptime(date, '%Y-%m-%d')
 
 
-# convert history into inputs and outputs
 def to_supervised(train, n_input, n_out=7):
     # flatten data
     data = train.reshape((train.shape[0] * train.shape[1], train.shape[2]))
@@ -37,69 +33,48 @@ def to_supervised(train, n_input, n_out=7):
 
 
 def build_model(train, n_input):
-    # prepare data
     train_x, train_y = to_supervised(train, n_input)
-    # define parameters
     verbose, epochs, batch_size = 0, 100, 1
     n_timesteps, n_features, n_outputs = train_x.shape[1], train_x.shape[2], train_y.shape[1]
-    # define model
     model = Sequential()
     model.add(LSTM(200, activation='relu', input_shape=(n_timesteps, n_features)))
     model.add(Dense(100, activation='relu'))
     model.add(Dense(n_outputs))
     model.compile(loss='mse', optimizer='adam')
-    # fit network
     model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=verbose)
     model.save('lstm_model')
     return model
 
 
 def evaluate_model(train, test, n_input):
-    # fit model
     model = build_model(train, n_input)
-    # history is a list of weekly data
     history = [x for x in train]
-    # walk-forward validation over each week
     predictions = list()
     for i in range(len(test)):
-        # predict the week
         yhat_sequence = forecast(model, history, n_input)
-        # store the predictions
         predictions.append(yhat_sequence)
-        # get real observation and add to history for predicting the next week
         history.append(test[i, :])
-    # evaluate predictions days for each week
     predictions = np.array(predictions)
     score, scores = evaluate_forecasts(test[:, :, 0], predictions)
     return score, scores
 
 
 def forecast(model, history, n_input):
-    # flatten data
     data = np.array(history)
     data = data.reshape((data.shape[0] * data.shape[1], data.shape[2]))
-    # retrieve last observations for input data
     input_x = data[-n_input:, 0]
-    # reshape into [1, n_input, 1]
     input_x = input_x.reshape((1, len(input_x), 1))
-    # forecast the next week
     yhat = model.predict(input_x, verbose=0)
-    # we only want the vector forecast
     yhat = yhat[0]
     return yhat
 
 
 def evaluate_forecasts(actual, predicted):
     scores = list()
-    # calculate an RMSE score for each day
     for i in range(actual.shape[1]):
-        # calculate mse
         mse = mean_squared_error(actual[:, i], predicted[:, i])
-        # calculate rmse
         rmse = sqrt(mse)
-        # store
         scores.append(rmse)
-    # calculate overall RMSE
     s = 0
     for row in range(actual.shape[0]):
         for col in range(actual.shape[1]):
--- prediction.py
+++ prediction.py
@@ -2,25 +2,18 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 from keras.models import load_model
-import matplotlib.pyplot as plt
 
 
-# Date Parser
 def parser(date):
     return dt.datetime.strptime(date, '%Y-%m-%d')
 
 
 def forecast(model, history, n_input):
-    # flatten data
     data = np.array(history)
     data = data.reshape((data.shape[0] * data.shape[1], data.shape[2]))
-    # retrieve last observations for input data
     input_x = data[-n_input:, 0]
-    # reshape into [1, n_input, 1]
     input_x = input_x.reshape((1, len(input_x), 1))
-    # forecast the next week
     yhat = model.predict(input_x, verbose=0)
-    # we only want the vector forecast
     yhat = yhat[0]
     return yhat
 
