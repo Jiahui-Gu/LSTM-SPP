3479cc1972fd9590c6e015a2dd6ec22d5177646a
Jayden <scalarequilibrium@gmail.com>
2020-07-07 23:59:55 +0800

    Updated Codes

--- main.py
+++ main.py
@@ -12,22 +12,17 @@ def parser(date):
 
 
 def to_supervised(train, n_input, n_out=7):
-    # flatten data
     data = train.reshape((train.shape[0] * train.shape[1], train.shape[2]))
     X, y = list(), list()
     in_start = 0
-    # step over the entire history one time step at a time
     for _ in range(len(data)):
-        # define the end of the input sequence
         in_end = in_start + n_input
         out_end = in_end + n_out
-        # ensure we have enough data for this instance
         if out_end <= len(data):
             x_input = data[in_start:in_end, 0]
             x_input = x_input.reshape((len(x_input), 1))
             X.append(x_input)
             y.append(data[in_end:out_end, 0])
-        # move along one time step
         in_start += 1
     return np.array(X), np.array(y)
 
